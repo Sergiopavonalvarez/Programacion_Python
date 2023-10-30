81af406a7f58f683e062ea3ffa3c1e1265321053
sergiopavonalvarez <sergiopavonalvarez@gmail.com>
2023-10-30 22:02:21 +0100

    commit 6

--- 00 Ejercicios/02 Ponte A Prueba 2/66.py
+++ 00 Ejercicios/02 Ponte A Prueba 2/66.py
@@ -1,29 +1,25 @@
 import sys
-
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QDial, \
-    QProgressBar
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QDial
+
 class Ventana(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Control de volumen")
         contenedor = QWidget()
-        self.etiqueta = QLabel("Valor: 0 dB")
-        self.etiqueta.setAlignment(Qt.AlignCenter)
+        self.etiqueta_db = QLabel("Valor: 0 dB")
+        self.etiqueta_db.setAlignment(Qt.AlignCenter)
         self.dial = QDial()
         self.dial.setMinimum(0)
         self.dial.setMaximum(100)
         self.dial.setValue(0)
-        self.indicador = QProgressBar()
-        self.indicador.setMaximum(100)
         self.boton_aumentar = QPushButton("+")
         self.boton_aumentar.setMaximumWidth(30)
         self.boton_disminuir = QPushButton("-")
         self.boton_disminuir.setMaximumWidth(30)
         layout = QVBoxLayout()
-        layout.addWidget(self.etiqueta)
+        layout.addWidget(self.etiqueta_db)
         layout.addWidget(self.dial)
-        layout.addWidget(self.indicador)
         layout.addWidget(self.boton_aumentar)
         layout.addWidget(self.boton_disminuir)
         contenedor.setLayout(layout)
@@ -32,14 +28,11 @@ class Ventana(QMainWindow):
         self.boton_aumentar.clicked.connect(self.aumentar_volumen)
         self.boton_disminuir.clicked.connect(self.disminuir_volumen)
     def actualizar_valor(self):
-        self.etiqueta.setText(f"Valor: {self.dial.value()} dB")
-        self.indicador.setValue(self.dial.value())
+        self.etiqueta_db.setText(f"Valor: {self.dial.value()} dB")
     def aumentar_volumen(self):
         self.dial.setValue(self.dial.value() + 1)
-        self.indicador.setValue(self.dial.value())
     def disminuir_volumen(self):
         self.dial.setValue(self.dial.value() - 1)
-        self.indicador.setValue(self.dial.value())
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana = Ventana()

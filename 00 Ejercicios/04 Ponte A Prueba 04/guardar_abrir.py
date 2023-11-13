3cafec1af684ee3dea35808168663a8db1c0a960
Sergio_Pavon <pavon_madrid999@hotmail.com>
2023-11-13 18:17:01 +0100

    18:16 13/11

--- 00 Ejercicios/04 Ponte A Prueba 04/guardar_abrir.py
+++ 00 Ejercicios/04 Ponte A Prueba 04/guardar_abrir.py
@@ -2,46 +2,28 @@ import sys
 from PySide6.QtCore import QCoreApplication, QFile, QIODevice
 from PySide6.QtGui import QAction, QIcon, QTextCursor
 from PySide6.QtWidgets import QApplication, QFileDialog, QTextEdit, QMainWindow
-
-
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Editor de texto plano")
-
-        # Define the text editor widget
         self.textEdit = QTextEdit()
         self.setCentralWidget(self.textEdit)
-
-        # Define the actions
-        self.openAction = QAction(QIcon("C://Users//pavon//Documents//PyCharm//Programacion_Python//00 Ejercicios//04 Ponte A Prueba 04//abrir.png"), "Abrir", self, shortcut="Ctrl+O")
-        self.saveAction = QAction(QIcon("C://Users//pavon//Documents//PyCharm//Programacion_Python//00 Ejercicios//04 Ponte A Prueba 04//descarga.png"), "Guardar", self, shortcut="Ctrl+S")
-
-
-        # Connect the actions to slots
+        self.openAction = QAction(QIcon("C:/Users/pavon/Documents/VS Code/Python/Programacion_Python/00 Ejercicios/04 Ponte A Prueba 04/abrir.png"), "Abrir", self, shortcut="Ctrl+O")
+        self.saveAction = QAction(QIcon("C:/Users/pavon/Documents/VS Code/Python/Programacion_Python/00 Ejercicios/04 Ponte A Prueba 04/descarga.png"), "Guardar", self, shortcut="Ctrl+S")
+        self.cerrarAction = QAction(QIcon("C:/Users/pavon/Documents/VS Code/Python/Programacion_Python/00 Ejercicios/04 Ponte A Prueba 04/salir.png"),"Cerrar archivo", self, shortcut="Ctrl+W", triggered=self.cerrar)
         self.openAction.triggered.connect(self.openFile)
         self.saveAction.triggered.connect(self.saveFile)
-
-
-        # Add the actions to the menu bar
+        self.cerrarAction.triggered.connect(self.cerrar)
         menuBar = self.menuBar()
         fileMenu = menuBar.addMenu("&Archivo")
         fileMenu.addAction(self.openAction)
         fileMenu.addAction(self.saveAction)
-
-
-        # Add the actions to the toolbar
+        fileMenu.addAction(self.cerrarAction)
         toolbar = self.addToolBar("Herramientas")
         toolbar.addAction(self.openAction)
         toolbar.addAction(self.saveAction)
-
-
-
-
-
-        # Show the window
+        toolbar.addAction(self.cerrarAction)
         self.show()
-
     def openFile(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Abrir archivo", "", "Text files (*.txt)")
         if filename:
@@ -52,18 +34,17 @@ class App(QMainWindow):
                 cursor.setDocument(self.textEdit.document())
                 cursor.insertText(text)
                 file.close()
-
     def saveFile(self):
-        filename = "archivo.txt"
+        filename = "archivo_04PonteAPrueba.txt"
         file = QFile(filename)
         if file.open(QIODevice.WriteOnly):
             text = self.textEdit.toPlainText()
             file.write(text.encode())
             file.close()
-
-
-
-
+    def cerrar(self):
+        global archivo_abierto
+        archivo_abierto = None
+        self.textEdit.setPlainText("")        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     editor = App()

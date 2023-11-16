a446d1bc6875e526ff84e3f8a587e7b2b7d88eff
Sergio_Pavon <pavon_madrid999@hotmail.com>
2023-11-16 18:51:19 +0100

    18: 51 16/11

---
+++ 03 Generacion de Interfaces Qt Designer/02/DI_U03_A03_05.py
@@ -0,0 +1,10 @@
+import sys
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+
+loader = QUiLoader()
+app = QApplication(sys.argv)
+window = loader.load("C:/Users/pavon/Documents/VS Code/Python/Programacion_Python/03 Generacion de Interfaces Qt Designer/02/untitled.ui", None)
+window.show()
+app.exec()

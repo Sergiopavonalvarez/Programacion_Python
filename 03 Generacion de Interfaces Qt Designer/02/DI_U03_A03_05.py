3c891e6c8e08ce2da87df632858c00aa681815ce
sergiopavonalvarez <aergiopavonalvarez@gmail.com>
2023-11-17 14:11:28 +0100

    14:11 17/11

--- 03 Generacion de Interfaces Qt Designer/02/DI_U03_A03_05.py
+++ 03 Generacion de Interfaces Qt Designer/02/DI_U03_A03_05.py
@@ -6,7 +6,8 @@ from PySide6.QtUiTools import QUiLoader
 loader = QUiLoader()
 app = QApplication(sys.argv)
 #window = loader.load("C:/Users/pavon/Documents/VS Code/Python/Programacion_Python/03 Generacion de Interfaces Qt Designer/02/untitled.ui", None)
-window = loader.load("/Users/sergiopavonalvarez/Documents/Documentos/VS Code/Python/Programacion_Python/03 Generacion de Interfaces Qt Designer/02/untitled.ui", None)
+#window = loader.load("/Users/sergiopavonalvarez/Documents/Documentos/VS Code/Python/Programacion_Python/03 Generacion de Interfaces Qt Designer/02/untitled.ui", None)
+window = loader.load("/Users/sergiopavonalvarez/Programacion/VS Code/Python/Programacion_Python/03 Generacion de Interfaces Qt Designer/02/untitled.ui", None)
 
 window.show()
 app.exec()

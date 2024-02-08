cd493f03b6d74a3a78b1ab8f2e05d2b63cbd67f4
sergiopavonalvarez <aergiopavonalvarez@gmail.com>
2024-02-08 14:50:31 +0100

    14:48 08/02

---
+++ 05 Sistema de Gestion Empresarial/04 Ejercicios/05 Masquecien.py
@@ -0,0 +1,10 @@
+
+
+numero1=int(input("Introduce un numero: "))
+numero2=int(input("Introduce otro numero: "))
+numero3=numero1+numero2
+if numero3 >100:
+    print("La suma supera la centena")
+if numero3 <100:
+    print("Suma: ",numero3)
+    print("El resultado de la suma no supera la centena")

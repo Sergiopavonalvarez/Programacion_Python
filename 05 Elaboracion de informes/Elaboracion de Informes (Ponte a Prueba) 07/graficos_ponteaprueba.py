f8415b7cf25d49d95d19383e6d7c4de9bff38c00
Sergio_Pavon <pavon_madrid999@hotmail.com>
2024-02-20 11:48:03 +0100

    11:47 20/02

---
+++ 05 Elaboracion de informes/Elaboracion de Informes (Ponte a Prueba) 07/graficos_ponteaprueba.py
@@ -0,0 +1,42 @@
+import datapane as dp
+import pandas as pd
+import matplotlib.pyplot as plt
+
+fichero_csv = "C:/Users/pavon/Documents/PyCharm/Programacion_Python/05 Elaboracion de informes/Elaboracion de Informes (Ponte a Prueba) 07/DI_U05_A02_PP_E_01.csv"
+df = pd.read_csv(fichero_csv)
+
+#Grafico Lineas
+ventas_mes = df.groupby(['Año'], sort=False).sum()
+grafico_matplot_lineas = ventas_mes.plot(y='Ventas')
+grafico_datapane_lineas = dp.Plot(grafico_matplot_lineas, responsive=False)
+
+
+#Grafico barras
+ventas_vendedor = df.groupby(['Región']).sum()
+grafico_matplot_barras = ventas_vendedor.plot.bar(y='Ventas')
+plt.tight_layout()
+grafico_datapane_barras = dp.Plot(grafico_matplot_barras, responsive=False)
+
+#Grafico tarta
+ventas_vendedors = df.groupby(['Tipo de producto']).sum()
+grafico_matplotlib_sectores = ventas_vendedors.plot.pie(y='Ventas', legend=False, ylabel="")
+grafico_datapane_sectores = dp.Plot(grafico_matplotlib_sectores, responsive=False)
+
+
+
+# Creamos un informe con los graficos:
+#report = dp.Report(grafico_datapane_lineas,grafico_datapane_barras,grafico_datapane_sectores)
+
+
+#Agrupados en dos columnas:
+report=dp.Report(
+    dp.Group(grafico_datapane_lineas, grafico_datapane_barras, grafico_datapane_sectores, columns=2)
+)
+
+
+
+
+
+#report.save(path="C:/Users/pavon/Documents/PyCharm/Programacion_Python/05 Elaboracion de informes/Elaboracion de Informes (Gráficos) 05/Informe_barras(Sinagrupar en columnas).html", open=True)
+report.save(path="C:/Users/pavon/Documents/PyCharm/Programacion_Python/05 Elaboracion de informes/Elaboracion de Informes (Ponte a Prueba) 07/Informe(Ponte a Prueba).html", open=True)
+

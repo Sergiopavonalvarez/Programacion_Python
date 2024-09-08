438df6e7cd60775d94c2cc0f8257b8407ff67804
Sergio Pavon <118288663+Sergiopavonalvarez@users.noreply.github.com>
2024-09-08 20:43:09 +0200

    20:42 08/09

--- 04 Componentes de Usuario/09 Integracion De Componentes En Herramientas De Diseño/DI_U04_A04_04_rc.py
+++ 04 Componentes de Usuario/09 Integracion De Componentes En Herramientas De Diseño/DI_U04_A04_04_rc.py
@@ -1,6 +1,6 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 6.5.2
+# Created by: The Resource Compiler for Qt version 6.6.2
 # WARNING! All changes made in this file will be lost!
 
 from PySide6 import QtCore
@@ -904,7 +904,7 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x8e\xd3\x0a\xaf1\
+\x00\x00\x01\x91\xd2\xf0\x02\xca\
 "
 
 def qInitResources():
